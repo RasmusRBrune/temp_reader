acea7cd600e7dbb83fb86684782c000c864afa45
jasper zoffmann nilsen <www.jasper@live.dk>
2023-11-14 13:33:09 +0100

    added migration with new entity

---
+++ temperature_Server/Migrations/TempReader/20231114123112_newEntityAdded.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace temperature_Server.Migrations.TempReader
+{
+    /// <inheritdoc />
+    public partial class newEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Account_AccountId",
+                table: "Devices");
+
+            migrationBuilder.CreateTable(
+                name: "TemperatureReaderDeviceKey",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemperatureReaderDeviceKey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TemperatureReaderDeviceKey_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemperatureReaderDeviceKey_DeviceId",
+                table: "TemperatureReaderDeviceKey",
+                column: "DeviceId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Account_AccountId",
+                table: "Devices",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Account_AccountId",
+                table: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "TemperatureReaderDeviceKey");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Account_AccountId",
+                table: "Devices",
+                column: "AccountId",
+                principalTable: "Account",
+                principalColumn: "Id");
+        }
+    }
+}
