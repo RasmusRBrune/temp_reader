ad2938538020c574cede9d98878ca49b23affa2a
jasper zoffmann nilsen <www.jasper@live.dk>
2023-11-14 09:58:18 +0100

    added device key and made it possible to make a device in db without having it paired to an account at first

---
+++ temperature_Server/Data/TemperatureReaderDeviceKey.cs
@@ -0,0 +1,10 @@
+﻿namespace temperature_Server.Data
+{
+	public class TemperatureReaderDeviceKey : BaseEntity<int>
+	{
+		public Guid DeviceId { get; set; }
+		public string Key { get; set; }
+		public TemperatureReaderDevice Device { get; set; }
+
+	}
+}
